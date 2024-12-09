571ecc6e691858af9165d04c63ccab5de8fe7784
Islem <ihadjkaddour7@gmail.com>
2024-12-09 22:56:33 +0100

    few fixed issues

--- app/src/main/java/com/example/averagecalculator/MainActivity.kt
+++ app/src/main/java/com/example/averagecalculator/MainActivity.kt
@@ -1,178 +1,152 @@
 package com.example.averagecalculator
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.averagecalculator.model.Module
-import com.example.averagecalculator.model.addModule
-import com.example.averagecalculator.model.buffer
-import com.example.averagecalculator.model.modules
-import com.example.averagecalculator.ui.theme.AverageCalculatorTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AverageCalculatorTheme {
-
-                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->}
-          /*  Screen()
-                LazyColumn  {
-                    items(modules){
-                        item->
-                        EduUnit(subject = item.subject, mark = item.mark, coeff = item.coeff)
-                    }
-                }*/
+            Surface(modifier = Modifier.padding(top=50.dp)) {
+                    MainScreen()
             }
         }
     }
 }
-
 @Composable
-fun EduUnit(subject: String,mark:Double,coeff:Int) {
-Box {
+    fun EduUnit(subject: String, mark: Int, coeff: Int) {
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(30.dp))
+                    .shadow(elevation = 4.dp)
+            )
+            {
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
 
-        Column(  modifier= Modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(30.dp))
-            // .padding(16.dp)
-            .shadow(elevation = 4.dp)
-        )
-        {
-            Spacer(modifier = Modifier.height(20.dp))
-            Row (
-                modifier=Modifier.fillMaxWidth(),
-
-            ){
-                Spacer(modifier = Modifier.width(25.dp))
-                Text(text = subject,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                Icon(imageVector = Icons.Outlined.Edit, contentDescription ="edit module" )
-                Spacer(modifier = Modifier.width(10.dp))
+                    ) {
+                    Spacer(modifier = Modifier.width(25.dp))
+                    Text(
+                        text = subject,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Icon(imageVector = Icons.Outlined.Edit, contentDescription = "edit module")
+                    Spacer(modifier = Modifier.width(10.dp))
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Row {
+                    Spacer(modifier = Modifier.width(25.dp))
+                    Text(
+                        text = "Grade: $mark"
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(text = "Coefficient: $coeff")
+                    Spacer(modifier = Modifier.width(50.dp))
+                }
+                Spacer(modifier = Modifier.height(20.dp))
             }
-            Spacer(modifier = Modifier.height(20.dp))
-            Row {
-                Spacer(modifier = Modifier.width(25.dp))
-                Text(text = "Grade: $mark"
-                   )
-
-                Spacer(modifier = Modifier.weight(1f))
-                Text(text = "Coefficient: $coeff")
-                Spacer(modifier = Modifier.width(50.dp))
-            }
-            Spacer(modifier = Modifier.height(20.dp))
         }
-}
-
-}
-
-
-//@Preview(showBackground = true)
-@Composable
-fun State(){
-    var state = remember {
-        mutableStateOf(0)
     }
-    Row (verticalAlignment = Alignment.CenterVertically
-    ){
-        Button(onClick = { state.value += 1 }) {
-            Text(text = state.value.toString())
-            Spacer(modifier = Modifier.width(20.dp))
-            Text(text = "add")
+@Composable
+    fun MainScreen() {
+        val modules = rememberSaveable { mutableStateOf(listOf<Module>()) }
+        Column {
+            ModuleBuffer(modules = modules)
+            LazyColumn {
+                items(modules.value) { item ->
+                    EduUnit(subject = item.subject, mark = item.mark, coeff = item.coeff)
+                }
+            }
+
         }
     }
 
-}
-
-
 @Composable
-fun Screen(){
-     val modules : MutableList<Module> = remember {
-         mutableListOf()
-     }
-    ModuleBuffer()
-}
+fun ModuleBuffer(modules:MutableState<List<Module>>) {
+    fun addModule(mod: Module) {
+        modules.value = modules.value + mod
 
-@Preview(showBackground = true)
-@Composable
-fun ScreenPreview(){
-    Screen()
-}
-
-@Composable
-fun ModuleBuffer() {
-        var buffer :Module = buffer
+    }
+    var subject by rememberSaveable {
+        mutableStateOf("")
+    }
+    var mark by rememberSaveable {
+        mutableStateOf("")
+    }
+    var coeff by rememberSaveable {
+        mutableStateOf("")
+    }
+    var showError by rememberSaveable {
+        mutableStateOf(false)
+    }
+    var weightedAverage by rememberSaveable {
+        mutableStateOf(0.0)
+    }
+    var showDialog by rememberSaveable {
+        mutableStateOf(false)
+    }
     Box {
-        val subject = remember {
-            mutableStateOf(buffer.subject)
-        }
-        val mark = remember{
-            mutableStateOf(buffer.mark.toString())
-        }
-        val coeff = remember {
-            mutableStateOf(buffer.coeff.toString())
-        }
-
-        Column(  modifier= Modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(30.dp))
-            //.padding(16.dp)
-            .shadow(elevation = 4.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(30.dp))
+                .shadow(elevation = 4.dp)
         )
         {
             Spacer(modifier = Modifier.height(20.dp))
-            Row (
-                modifier=Modifier.fillMaxWidth(),
-                ){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
                 Spacer(modifier = Modifier.width(25.dp))
                 Text(text = "Name:")
                 Spacer(modifier = Modifier.width(5.dp))
-
-                TextField(value = subject.value, onValueChange = {newSubject->
-                    subject.value = newSubject
+                TextField(value = subject, onValueChange = { newSubject ->
+                    subject = newSubject
                 })
 
                 Spacer(modifier = Modifier.width(10.dp))
@@ -180,32 +154,103 @@ fun ModuleBuffer() {
             Spacer(modifier = Modifier.height(20.dp))
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Spacer(modifier = Modifier.width(25.dp))
-
                 Text(text = "Grade:")
                 Spacer(modifier = Modifier.width(10.dp))
-                OutlinedTextField(modifier= Modifier
+                OutlinedTextField(modifier = Modifier
                     .width(55.dp)
                     .height(50.dp),
-
-                    value = mark.value, onValueChange = {newMark ->
-                        mark.value = newMark})
+                    value = mark, onValueChange = { newMark ->
+                        mark = newMark
+                    })
                 Spacer(modifier = Modifier.weight(1f))
                 Text(text = "Coefficient:")
                 Spacer(modifier = Modifier.width(10.dp))
-                OutlinedTextField(modifier= Modifier
-                    .width(50.dp)
-                    .height(50.dp),
-
-                    value = coeff.value, onValueChange = {newCoeff ->
-                    coeff.value = newCoeff})
+                OutlinedTextField(
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(50.dp),
+                    value = coeff,
+                    onValueChange = { newCoeff ->
+                        coeff = newCoeff
+                    },
+                )
                 Spacer(modifier = Modifier.width(50.dp))
             }
             Spacer(modifier = Modifier.height(5.dp))
-            Button(onClick = { addModule(buffer) },Modifier.padding(10.dp)) {
-                Text(text = "Submit")
+            if (showError) {
+                Text(
+                    text = "Error: invalid input or duplicate module name",
+                    color = Color.Red,
+                    modifier = Modifier.padding(start = 40.dp)
+                )
+            }
+            Row {
+                Button(
+                    onClick = {
+                        val isDuplicate = modules.value.any() { it.subject == subject }
+                        val isValideGrade = mark.toDoubleOrNull() != null
+                        val isValidCoefficient = coeff.toDoubleOrNull() != null
+                        if (!isValidCoefficient || isDuplicate|| !isValideGrade || coeff.equals(0)) {
+                            showError = true
+                        } else {
+                            val newModule = Module(subject, mark.toInt(), coeff.toInt())
+                            addModule(mod = newModule)
+                            subject=""
+                            mark=""
+                            coeff=""
+                            showError= false
+                        }
+                    },
+                    Modifier.padding(10.dp),
+                    enabled = (subject.isNotBlank() && mark.isNotBlank() && coeff.isNotBlank())
+                ) {
+                    Text(text = "Submit")
+                }
+                Spacer(Modifier.weight(1f))
+
+                Button(
+                    onClick = {
+                        /*TODO*/weightedAverage = calculateWeightedAverage(modules = modules.value)
+                        showDialog = true
+                    },
+                    Modifier.padding(10.dp),
+                    enabled = modules.value.size>1
+                ) {
+                    Text(text = "calculate")
+                }
+                Spacer(Modifier.width(40.dp))
+            }
+            if (showDialog){
+                weightedAverageDialog(average = weightedAverage, onDismiss = {showDialog = false})
+
             }
         }
     }
-
-
 }
+
+@Composable
+fun weightedAverageDialog(average : Double , onDismiss : ()->Unit) {
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+                Button(
+                    onClick = { onDismiss() },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White,
+                        contentColor = Color.Magenta
+                    )
+                ) {
+                Text(text = "OK")
+            }
+        },
+        title = { Text(text = "sememster average")},
+        text = { Text(text = "your average is $average")},
+
+        )
+}
+
+fun calculateWeightedAverage(modules: List<Module>): Double {
+    return modules.sumOf { it.mark * it.coeff }.toDouble()/modules.sumOf { it.coeff }
+}
+
+
--- app/src/main/java/com/example/averagecalculator/model/Module.kt
+++ app/src/main/java/com/example/averagecalculator/model/Module.kt
@@ -1,29 +1,15 @@
 package com.example.averagecalculator.model
 
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-
 data class Module(
-    val subject :String,
-    val mark : Double,
-    val coeff : Int
-)
-var buffer : Module = Module(
-    "",
-    0.0,
-    0
+        var subject :String,
+        val mark : Int,
+        val coeff : Int
 )
 
 
-var modules: MutableList<Module> = mutableListOf()
 
 
 
-fun addModule(module: Module){
-    if(module.subject!="" && module.subject!=null && module.mark!= 0.0 && module.coeff!=0){
-        modules.add(module)
-        return
-    }
-    print("incomplete module")
-}
+
+
 
